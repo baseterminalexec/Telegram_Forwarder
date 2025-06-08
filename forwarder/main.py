c97bb1cd1a95bd0f5db900bc1d47dc9ca5bd8577
baseterminalexec <baseterminal@protonmail.com>
2025-06-08 18:21:01 +0100

    Update main.py

--- forwarder/main.py
+++ forwarder/main.py
@@ -6,8 +6,11 @@ from forwarder.modules import ALL_MODULES
 for module in ALL_MODULES:
     importlib.import_module("forwarder.modules." + module)
 
-
 def run():
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
     LOGGER.info("Starting bot...")
-    bot.run_polling()
+
+    try:
+        bot.run_polling()
+    except Exception as e:
+        LOGGER.error(f"Bot stopped due to: {e}")
